d9af8e8457054eac6a17ead5e14050db11860b1f
arghavan99 <a.rezvani99@yahoo.com>
2018-08-18 15:09:46 +0430

    appsCreated

---
+++ backend/register/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here
+def profile_info(request):
+    token = request.POST.get('token')
+    user = token.user
+    profile = user.profile
+    
