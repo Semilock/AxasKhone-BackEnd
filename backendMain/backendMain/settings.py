e5c0bc8933d22dfb95f243537e659b761eeeb61f
Kiana.G <kiana.gholampoor@gmail.com>
2018-08-19 14:31:26 +0430

    login methods! it works! :))

--- backendMain/backendMain/settings.py
+++ backendMain/backendMain/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
 
     'rest_framework',
+
 ]
 
 
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- backendMain/user/views.py
+++ backendMain/user/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 from backendMain import serializers
 
 
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from user.models import Token
+from uuid import uuid4
+
+
 def profile_info(request):
     token = request.POST.get('token')
     user = token.user
@@ -13,4 +19,25 @@ def profile_info(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+def login(request):
+    try:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print ("username" , username)
+        print ("password" , password)
+
+        user= User.objects.get(username= username)
+        #return JsonResponse({"username" : user.username})
+
+        if( not user.check_password(password)):
+            return JsonResponse({"error" : "bad_password" ,})
+        token = Token(user= user.profile , value=  str(uuid4()))
+       # print("token" , token.value)
+        return JsonResponse({"Token": token.value} ,)
+
+    except User.DoesNotExist:
+        return JsonResponse({"error" : "bad_username"})
 
