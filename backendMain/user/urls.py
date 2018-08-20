2e843d20cc8efba4f1b15d636019bc3e4c3e51e5
arghavan99 <a.rezvani99@yahoo.com>
2018-08-20 21:36:51 +0430

    commenting

--- backendMain/user/urls.py
+++ backendMain/user/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = {
     url(r'^register/', views.register, name='register'),
-    url(r'^register-complement/', views.UserViewSet.as_view({'post': 'update'})),
-    url(r'^change_password/', views.change_password, name='change_password'),
+    # url(r'^register-complement/', views.UserViewSet.as_view({'post': 'update'})),
+    # url(r'^change_password/', views.change_password, name='change_password'
 }
