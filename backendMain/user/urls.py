d854e156ec74f279c28afaf96f1b12968abc2daa
Kiana.G <kiana.gholampoor@gmail.com>
2018-08-21 12:24:58 +0430

    change password with JWT implemented

--- backendMain/user/urls.py
+++ backendMain/user/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-
 urlpatterns = {
+
     url(r'^register/', views.register, name='register'),
+    # url(r'^change_password/', views.change_password, name='change_password'),
     # url(r'^register-complement/', views.UserViewSet.as_view({'post': 'update'})),
-    # url(r'^change_password/', views.change_password, name='change_password'
+    url(r'^change_password/', views.change_password, name='change_password')
 }
