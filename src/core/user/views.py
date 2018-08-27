43a2e436be8332f21fb230e23291d161088b5fe3
Kiana.G <kiana.gholampoor@gmail.com>
2018-08-27 19:17:25 +0430

    refactor :)))

--- src/core/user/views.py
+++ src/core/user/views.py
@@ -131,40 +131,40 @@ class ChangePassword(APIView):
         return JsonResponse({"status": "succeeded"})
 
 
-class ProfileInfo(APIView):
-    #TODO: more details for profile should return + profile pic
-    """"
-    this should show profile of user
-    """
-    def get(self, request):
-        user = request.user
-        profile = Profile.objects.get(user=user)
-        serializer = ProfileSerializerGet(profile)
-        return JsonResponse(serializer.data)
-
-    """
-          this will let user change profile info
-       """
-
-    def post(self, request):
-        if not request.user:
-            return Response({'status': 'failed'})
-        new_username = request.data.get("username")
-        new_bio = request.data.get("bio")
-        new_email = request.data.get("email")
-        new_fullname = request.data.get("fullname")
-        #       todo add profile picture field for update
-        # todo check validation
-        user = request.user
-        user.email = new_email
-        user.username = new_email
-        profile = Profile.objects.get(user=user)
-        profile.fullname = new_fullname
-        profile.bio = new_bio
-        profile.main_username = new_username
-        user.save()
-        profile.save()
-        return Response({'status': 'succeeded'})
+# class ProfileInfo(APIView):
+#     #TODO: more details for profile should return + profile pic
+#     """"
+#     this should show profile of user
+#     """
+#     def get(self, request):
+#         user = request.user
+#         profile = Profile.objects.get(user=user)
+#         serializer = ProfileSerializerGet(profile)
+#         return JsonResponse(serializer.data)
+#
+#     """
+#           this will let user change profile info
+#        """
+#
+#     def post(self, request):
+#         if not request.user:
+#             return Response({'status': 'failed'})
+#         new_username = request.data.get("username")
+#         new_bio = request.data.get("bio")
+#         new_email = request.data.get("email")
+#         new_fullname = request.data.get("fullname")
+#         #       todo add profile picture field for update
+#         # todo check validation
+#         user = request.user
+#         user.email = new_email
+#         user.username = new_email
+#         profile = Profile.objects.get(user=user)
+#         profile.fullname = new_fullname
+#         profile.bio = new_bio
+#         profile.main_username = new_username
+#         user.save()
+#         profile.save()
+#         return Response({'status': 'succeeded'})
 
 
 class UsersViewApi(APIView):
