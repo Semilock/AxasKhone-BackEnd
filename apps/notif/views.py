58aea0757ff63bd34d610ef08c00db3399468a8c
arghavan99 <a.rezvani99@yahoo.com>
2018-09-11 13:14:59 +0430

    allow only localhost

--- apps/notif/views.py
+++ apps/notif/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView
-import json
+from rest_framework.views import APIView, status
 
 
 @permission_classes((AllowAny,))
 class SaveToDataBase(APIView):
     def post(self, request):
-        print(request.data.get("data"))
-        return Response()
+        print(request.META.get("REMOTE_ADDR"))
+        if request.META.get("REMOTE_ADDR")=="127.0.0.1":
+            print(request.data.get("data"))
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
