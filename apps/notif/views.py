54c244f9acfc7d4a7595d7176f6bdaea006de588
arghavan99 <a.rezvani99@yahoo.com>
2018-09-15 00:54:51 +0430

    notif app logging(1)

--- apps/notif/views.py
+++ apps/notif/views.py
@@ -1,25 +1,34 @@
+import logging
+
+from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.utils.translation import gettext as _
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
-from rest_framework.views import APIView, status
-from django.utils.translation import gettext as _
-from apps.notif.models import Notification
-from core.user.models import Profile, UserFollow
+from rest_framework.views import APIView
+from rest_framework.views import status
+
 from Redis.globals import *
+from apps.notif.models import Notification
+from config.utils import now_ms, req_log_message, res_log_message
+from core.user.models import Profile, UserFollow
 from core.user.views import ForgotPassword, VerificationRequest
 
+logger = logging.getLogger(__name__)
 
 @permission_classes((AllowAny,))
 class RedisActions(APIView):
     def post(self, request):
+        req_time = now_ms()
+        logger.info(req_log_message(request, req_time))
         if str(request.META.get("REMOTE_ADDR")) == "127.0.0.1":
             type = request.data.get("type")
             if type==forgot_password_type:
-                return self.forgot_pass_mail(request)
+                return self.forgot_pass_mail(request, req_time)
             if type == email_verification_type:
-                return self.verify_email(request)
+                return self.verify_email(request, req_time)
             receiver = request.data.get("receiver")
             sender = request.data.get("sender")
             object = request.data.get("object")
@@ -36,8 +45,14 @@ class RedisActions(APIView):
                 notif.save()
             if type == follow_type:
                 self.create_notif_for_all_followers(object, sender, type)
+            log_result = 'user(id={0}) notif successfully handled from redis queue'.format(sender.id)
+            log_message = res_log_message(request, log_result, req_time)
+            logger.info(log_message)
             return Response(status=status.HTTP_200_OK)
         else:
+            log_result = 'try to send request to internal api of redis'
+            log_message = res_log_message(request, log_result, req_time)
+            logger.error(log_message)
             return Response(status=status.HTTP_403_FORBIDDEN)
 
     def create_notif_for_all_followers(self, object, sender, type):
@@ -54,7 +69,7 @@ class RedisActions(APIView):
                                      you=False)
                 notif.save()
 
-    def forgot_pass_mail(self, request):
+    def forgot_pass_mail(self, request, req_time):
         username = request.data.get("username")
         email = request.data.get("email")
         url = request.data.get("url")
@@ -63,13 +78,19 @@ class RedisActions(APIView):
                                                      email,
                                                      url)
         if send_mail_response_code == 200:
+            log_result = 'forgot password mail successfully sent to user(id={0})'.format(User.objects.get(profile__main_username=username))
+            log_message = res_log_message(request, log_result, req_time)
+            logger.info(log_message)
             return JsonResponse({"status": _("Succeeded. Please check your email.")},
                                 status=HTTP_200_OK)
         else:
+            log_result = 'fail to send forgot password mail to user(id={0})'.format(User.objects.get(profile__main_username=username))
+            log_message = res_log_message(request, log_result, req_time)
+            logger.warning(log_message)
             return JsonResponse({"error": _("Failure in sending email. Try later")},
                                 status=send_mail_response_code)
 
-    def verify_email(self, request):
+    def verify_email(self, request, req_time):
         username = request.data.get("username")
         email = request.data.get("email")
         url = request.data.get("url")
@@ -78,8 +99,16 @@ class RedisActions(APIView):
                                                         email,
                                                         url)
         if send_mail_response_code == 200:
+            log_result = 'verification mail successfully sent to user(id={0})'.format(
+                User.objects.get(profile__main_username=username))
+            log_message = res_log_message(request, log_result, req_time)
+            logger.info(log_message)
             return JsonResponse({"status": _("Succeeded. Please check your email.")},
                                 status=HTTP_200_OK)
         else:
+            log_result = 'fail to send verification mail to user(id={0})'.format(
+                User.objects.get(profile__main_username=username))
+            log_message = res_log_message(request, log_result, req_time)
+            logger.warning(log_message)
             return JsonResponse({"error": _("Failure in sending email. Try later")},
                                 status=send_mail_response_code)
