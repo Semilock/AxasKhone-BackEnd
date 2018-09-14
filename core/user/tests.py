8dd3db8b3f63e5037666ecb235dbfb86e62f50a3
Kiana.G <kiana.gholampoor@gmail.com>
2018-09-14 18:00:55 +0430

    test - not complete

--- core/user/tests.py
+++ core/user/tests.py
@@ -3,8 +3,33 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework.utils import json
 
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework_simplejwt.settings import api_settings
 
-class RegistrAPIViewTestCase(APITestCase):
+from core.user.models import Profile
+
+
+class APIJWTClient(APIClient):
+    def login(self, url=reverse("login"), get_response=False, token="access", auth_header_type=0, **credentials):
+
+        auth_header_type = auth_header_type if auth_header_type < len(api_settings.AUTH_HEADER_TYPES) else 0
+        response = self.post(url, credentials, format='json')
+        if response.status_code == status.HTTP_200_OK:
+            self.credentials(
+                HTTP_AUTHORIZATION="{0} {1}".format(
+                    api_settings.AUTH_HEADER_TYPES[auth_header_type] if isinstance(auth_header_type, int) else auth_header_type,
+                    response.data[token]))
+            return (True, response) if get_response else True
+        else:
+            return (False, response) if get_response else False
+
+
+class APIJWTTestCase(APITestCase):
+    client_class = APIJWTClient
+
+
+class RegistrAPIViewTestCase(APIJWTTestCase):
     url = reverse("register")
 
     def test_invalid_password(self):
@@ -84,7 +109,7 @@ class RegistrAPIViewTestCase(APITestCase):
         response = self.client.post(self.url, user_data_2)
         self.assertEqual(400, response.status_code)
 
-class LoginAPIViewTestCase(APITestCase):
+class LoginAPIViewTestCase(APIJWTTestCase):
     url = reverse("login")
 
     def setUp(self):
@@ -92,6 +117,12 @@ class LoginAPIViewTestCase(APITestCase):
         self.email = "john@snow.com"
         self.password = "you_know_nothing"
         self.user = User.objects.create_user(self.username, self.email, self.password)
+
+    def test_login(self):
+        result, response = self.client.login(username=self.username, password=self.password, get_response=True)
+        # print (response.content)
+        self.assertEqual(True, result)
+
 
     def test_authentication_without_password(self):
         response = self.client.post(self.url, {"username": "snowman@gmail.com"})
@@ -110,3 +141,52 @@ class LoginAPIViewTestCase(APITestCase):
         print(self.username , self.password)
         self.assertEqual(200, response.status_code)
 
+
+class ProfileInfoAPIViewTestCase(APIJWTTestCase):
+    url = reverse("user:profile_info")
+    user_data = {
+        "username": "edituser",
+        "email": "edittest@testuser.com",
+        # "password": "long and hard password!",
+        "fullname": "editfullname",
+        "bio": "edit bio"
+    }
+
+    def setUp(self):
+        self.username = "john@snow.com"
+        self.email = "john@snow.com"
+        self.password = "you_know_nothing"
+        self.fullname = "john stark"
+        self.bio = "my bio"
+        self.user = User.objects.create_user(self.username, self.email, self.password)
+        self.user.profile = Profile.objects.create(self.fullname, self.email, self.password)
+
+    def login(self):
+        self.client.login(username=self.username, password=self.password, get_response=True)
+
+    # def test_create_profile(self):
+    #     # self.profile = Profile.objects.create(user_data)
+    #     response = self.client.post(self.url)
+    #     self.assertEqual(200, response.status_code)
+
+    # def test_get_profile_info(self):
+    #
+    #     """
+    #         Test to verify profile object bundle
+    #     """
+    #     response = self.client.get(self.url)
+    #     self.assertEqual(200, response.status_code)
+    #     print(response.content)
+    #
+    #     # profile_serializer_data = [ProfileSerializer(instance=self.user.profile).data
+    #     # response_data = json.loads(response.content)
+    #     # self.assertEqual(profile_serializer_data, response_data)
+    #     #
+    #     #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
