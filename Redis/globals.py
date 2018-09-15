9058dab5ebfa9adcace9fda6e249d36e6f2b901e
arghavan99 <a.rezvani99@yahoo.com>
2018-09-16 01:44:17 +0430

    send mail for invite added to queue

--- Redis/globals.py
+++ Redis/globals.py
@@ -8,6 +8,7 @@ queue = SimpleQueue(conn, 'queue')
 comment_type = 'comment'
 like_type = 'like'
 forgot_password_type = 'forgot'
+invite_mail_type = 'invite_mail'
 email_verification_type = 'verification'
 follow_type = 'follow'
 follow_request_type = 'follow_request'
